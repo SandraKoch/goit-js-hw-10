d02eb9bebc49d30e5d6b66dac7f6132b183416f9
SandraKoch <sandraannakoch@gmail.com>
2023-05-12 23:00:19 +0200

    js update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import _, { debounce } from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+console.log(inputEl);
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(res => res.json())
+    .then(object =>
+      object.map(item => {
+        return {
+          name: item.name.official,
+          capital: item.capital[0],
+          population: item.population,
+          flags: item.flags.svg,
+          languages: item.languages,
+        };
+      })
+    );
+}
+
+inputEl.addEventListener('input', () => {
+  console.log(inputEl.value);
+  _.debounce(
+    fetchCountries(inputEl.value).then(result => console.log(result)),
+    DEBOUNCE_DELAY
+  );
+});
