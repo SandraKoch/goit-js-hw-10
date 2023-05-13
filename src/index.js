3b36c43f37816686c3d3eb368e9bf6685bfd5da6
SandraKoch <sandraannakoch@gmail.com>
2023-05-13 23:10:26 +0200

    css styles update, country info card fix, Notiflix implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './css/styles.css';
-// var debounce = require('lodash.debounce');
 import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
 function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}`)
@@ -13,7 +15,7 @@ function fetchCountries(name) {
       return res.ok ? res.json() : [];
     })
     .then(object => {
-      // console.log('object check', object);
+      // console.log('object check', name, object);
       return object.map(item => {
         return {
           name: item.name.official,
@@ -27,27 +29,57 @@ function fetchCountries(name) {
 }
 
 function handleResults(results) {
-  console.log('results', results);
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
   if (results.length > 10) {
-    //notiflix "Too many matches found. Please enter a more specific name."
-    console.log(
-      'Too many matches found. Please enter a more specific name.',
-      results
-    );
+    Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (results.length >= 2 && results.length <= 10) {
-    //display function
-    console.log('display results');
+    displayCountryList(results);
+  } else if (results.length === 1) {
+    const CountryObj = results[0];
+    displayCountryInfo(CountryObj);
   } else {
-    //display card
-    console.log('display card');
+    Notify.failure('Oops, there is no country with that name');
   }
+}
+
+function displayCountryList(results) {
+  for (let i = 0; i < results.length; i++) {
+    const country = results[i];
+    const countryFlag = country.flags;
+    const countryName = country.name;
+
+    // console.log('loop results', country);
+    const instance = document.createElement('li');
+    instance.insertAdjacentHTML(
+      'beforeend',
+      `<div><img src="${countryFlag}"><span>${countryName}</span></div>`
+    );
+    countryListEl.append(instance);
+  }
+}
+
+function displayCountryInfo(result) {
+  // console.log('country info', result);
+
+  const countryFlag = result.flags;
+  const countryName = result.name;
+  const countryCapital = result.capital;
+  const countryPopulation = result.population;
+  const countryLanguages = result.languages;
+  countryInfoEl.insertAdjacentHTML(
+    'beforeend',
+    `<div class="country-main-info"><img src="${countryFlag}"><b><span class="country-name">${countryName}</span></b></div>
+    <p><b>Capital: </b>${countryCapital}</p>
+    <p><b>Population: </b>${countryPopulation}</p>
+    <p><b>Languages: </b>${Object.values(countryLanguages)}</p>`
+  );
 }
 
 inputEl.addEventListener(
   'input',
   debounce(() => {
     const trimmedInputValue = inputEl.value.trim();
-    console.log(trimmedInputValue);
     fetchCountries(trimmedInputValue).then(result => {
       handleResults(result);
     });
